36d7f477ab6adb41a924650444cff4dcc72e653d
Dharshni10 <dharshni0610@gmail.com>
2026-06-17 20:36:29 +0530

    Implemented resume parser

--- backend/resume_parser.py
+++ backend/resume_parser.py
@@ -1,21 +1,6 @@
-import pdfplumber
+from pdfminer.high_level import extract_text
 
-def extract_text(pdf_path):
-    text = ""
+text = extract_text("test_files/sample_resume.pdf")
 
-    with pdfplumber.open(pdf_path) as pdf:
-        for page in pdf.pages:
-            page_text = page.extract_text()
-
-            if page_text:
-                text += page_text + "\n"
-
-    return text
-
-
-if __name__ == "__main__":
-    pdf_file = "sample_resume.pdf"  # replace with your resume pdf
-
-    extracted_text = extract_text(pdf_file)
-
-    print(extracted_text)
+print("Length:", len(text))
+print(text)
